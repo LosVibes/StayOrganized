93a40ddf5a84e68e8d4e99087977a0aee4c16814
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-12-08 11:44:11 -0600

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
+const host = "http://35.165.18.146"
+// const host = "http://localhost:8083"
 
 const userOption = u => `<option value="${u.id}">${u.name} - (${u.username})</option>`;
-function fillUserList() {
-    fetch("http://localhost:8083/api/users")
-        .then(r => r.json())
-        .then(users => users.forEach(u => userList.innerHTML += userOption(u)));
+function fillUserList(){
+    fetch(host + "/api/users")
+    .then(r => r.json())
+    .then(users => users.forEach(u => userList.innerHTML += userOption(u)));
 }
--- scripts/new_todo.js
+++ scripts/new_todo.js
@@ -2,22 +2,24 @@ document.addEventListener("DOMContentLoaded", e => {
 
     fillUserList();
 
-    const catOption = c => `<option value="${c.id}">${c.name}</option>`;
+    const catOption = c => `<option>${c.name}</option>`;
+    //REMOVE VALUE SO THE DISPLAY NAME WILL BE USED BY THE NEW TODO
+    // const catOption = c => `<option value="${c.id}">${c.name}</option>`;
 
-    fetch("http://localhost:8083/api/categories")
+    fetch( host + "/api/categories")
         .then(r => r.json())
         .then(cats => cats.forEach(c => catList.innerHTML += catOption(c)));
 
-    //// GOAL IS TO GENERATE AN ENCODED FORM TO SEND AS "BODY"
-    //userid=1&category=1&priority=Medium&description=Make+Tea&deadline=2023-12-06 
+    //// GOAL IS TO GENERATE AN ENCODED FORM TO SEND AS THE "body"    
+    //userid=1&category=1&priority=Medium&description=Make+Tea&deadline=2023-12-06
 
     saveButton.addEventListener("click", e => {
 
-        //// LOOP WAY OF GATHERING DATA
+        // LOOP WAY OF GATHERING FORM DATA
         const elements = document.querySelectorAll("main [name]");
-        const content = [...elements].map(e => `${e.name}=${ encodeURIComponent(e.value) }`).join("&");
+        const content = [...elements].map(e => `${encodeURIComponent(e.name)}=${encodeURIComponent(e.value)}`).join("&");
 
-        // LINEAR WAY IF GATHERING FROM DATA
+        // LINEAR WAY OF GATHERING FORM DATA
         // const content = new FormData();
         // content.append("userid", userList.value);
         // content.append("category", catList.value);
@@ -25,14 +27,16 @@ document.addEventListener("DOMContentLoaded", e => {
         // content.append("description", description.value);
         // content.append("deadline", deadline.value);
         // console.log(content)
-        //// FormData() NOT WORKING WITH ANY OR NON FOR CONTENT TYPE
-        
-        fetch("http://localhost:8083/api/todos/", {method:"POST", body: content, 
-        headers:{
-            "Content-type": "application/x-www-form-urlencoded" // "application/json"
-        } 
-    }).then(r => {
-        location="todos.html"
-    });
-    });// END SAVE
-});// END LOADED
+        //// FormData() NOT WORKING WITH ANY OR NONE FOR CONTENT TYPE
+
+        fetch(host + "/api/todos/", {
+            method: "POST", body: content,
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded" //"application/json"
+            }
+        }).then(r => {
+            location="/todos.html"
+        });
+
+    }); //END SAVE
+});//END LOADED
